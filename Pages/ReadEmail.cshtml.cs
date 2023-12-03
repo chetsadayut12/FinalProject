9ad988270a2fe520bc75a52428053ace52271938
CHETSADAYUT SAPHA <chetsadayut.saph@bu.ac.th>
2023-12-03 22:52:06 +0700

    เสร็จ 100%

--- Pages/ReadEmail.cshtml.cs
+++ Pages/ReadEmail.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Data.SqlClient;
+using System;
 
 namespace FinalProject.Pages
 {
@@ -12,45 +13,55 @@ namespace FinalProject.Pages
         public string EmailDate { get; set; }
         public string EmailContent { get; set; }
 
-        public void OnGet(string emailid)
-        {
-            PopulateEmailData(emailid);
-        }
-
-        private void PopulateEmailData(string emailid)
+        public IActionResult OnGet(string emailid)
         {
             try
             {
-                String connectionString = "Server=tcp:abcdgroup.database.windows.net,1433;Initial Catalog=DBforProjectCS;Persist Security Info=False;User ID=abcd;Password=cs4361234.;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                string connectionString = "Server=tcp:abcdgroup.database.windows.net,1433;Initial Catalog=DBforProjectCS;Persist Security Info=False;User ID=abcd;Password=cs4361234.;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"; // Replace with your actual connection string
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
 
-                    String sql = "SELECT * FROM emails WHERE EmailID = @EmailID";
-                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    string selectSql = "SELECT * FROM emails WHERE EmailID = @EmailID";
+                    using (SqlCommand selectCommand = new SqlCommand(selectSql, connection))
                     {
-                        command.Parameters.AddWithValue("@EmailID", emailid);
+                        selectCommand.Parameters.AddWithValue("@EmailID", emailid);
 
-                        using (SqlDataReader reader = command.ExecuteReader())
+                        using (SqlDataReader reader = selectCommand.ExecuteReader())
                         {
                             if (reader.Read())
                             {
-                                EmailID = "" + reader.GetInt32(0);
+                                EmailID = reader.GetInt32(0).ToString();
                                 EmailSubject = reader.GetString(1);
                                 EmailContent = reader.GetString(2);
                                 EmailDate = reader.GetDateTime(3).ToString();
                                 EmailSender = reader.GetString(5);
+
+                                reader.Close();
+
+                                // Mark the email as read (update EmailIsRead to 1)
+                                string updateSql = "UPDATE emails SET emailisread = 1 WHERE EmailID = @EmailID";
+                                using (SqlCommand updateCommand = new SqlCommand(updateSql, connection))
+                                {
+                                    updateCommand.Parameters.AddWithValue("@EmailID", emailid);
+                                    updateCommand.ExecuteNonQuery();
+                                }
+
+                                return Page();
                             }
                         }
                     }
                 }
+
+                // Email not found
+                return NotFound();
             }
             catch (Exception ex)
             {
+                // Log the exception or handle it as needed
                 Console.WriteLine(ex.ToString());
+                return StatusCode(500, "An error occurred while processing your request.");
             }
         }
     }
-
-
 }
